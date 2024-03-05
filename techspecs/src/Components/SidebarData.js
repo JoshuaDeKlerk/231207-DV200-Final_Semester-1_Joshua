55616a63a3399e9c6c19e0bd9b8ddde41d3c09d7
JoshuaDeKlerk <231207@virtualwindow.co.za>
2024-03-05 09:56:18 +0200

    Nav

---
+++ techspecs/src/Components/SidebarData.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { MdDashboard } from "react-icons/md";
+import { LuGitCompare } from "react-icons/lu";
+import { RiTimeLine } from "react-icons/ri";
+
+export const SideBarData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: <MdDashboard />,
+    cName: 'nav-text',
+  },  
+  {
+    title: 'Compare',
+    path: '/Compare',
+    icon: <LuGitCompare />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'Timeline',
+    path: '/Timeline',
+    icon: <RiTimeLine />,
+    cName: 'nav-text',
+  },    
+]
